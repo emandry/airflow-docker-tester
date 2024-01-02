833c277456fb489cc1613d4d47cf3e3b1f2df21a
amaclemandry <emandry@smu.cl>
2024-01-02 15:12:14 -0300

    Include consul to read variables and import it into airflow

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN echo https://dl-cdn.alpinelinux.org/alpine/edge/main >> /etc/apk/repositorie
 RUN echo https://dl-cdn.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories
 
 ENV TZ=UTC
-
+RUN apk update && apk upgrade
 RUN apk add --no-cache \
         build-base \
         cmake \
@@ -22,17 +22,32 @@ RUN apk add --no-cache \
         bash \
         py3-pybind11 \
         py3-pybind11-dev \
-        geos 
+        py3-virtualenv \
+        geos wget libc++ libc++-dev re2-dev re2 geos-dev ca-certificates
         
-RUN apk add --no-cache libc++ libc++-dev re2-dev re2 geos-dev
-RUN apk update && apk upgrade
+#RUN apk add --no-cache wget libc++ libc++-dev re2-dev re2 geos-dev ca-certificates
 
-RUN pip3 install --no-cache-dir cython wheel pytest flake8 --break-system-packages
-RUN pip3 install --no-cache-dir pytest-black numpy datetime geos --break-system-packages
-RUN pip3 install --no-cache-dir apache-airflow[google] --break-system-packages
 
-COPY entrypoint.sh /
-RUN chmod +x /entrypoint.sh 
+## Add Certificate Validation and consul
+RUN update-ca-certificates
+
+RUN pip3 install --no-cache-dir cython wheel pytest --break-system-packages
+RUN pip3 install --no-cache-dir numpy datetime geos --break-system-packages
+RUN pip3 install --no-cache-dir apache-airflow[google,cncf.kubernetes] --break-system-packages
+
+RUN mkdir /entrypoint
+COPY consul/* /entrypoint
+
+ARG CONSUL_TEMPLATE_VERSION=0.35.0
+RUN cd /entrypoint && wget "https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip" && \
+  unzip consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip && \
+  rm -f consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip
+  
+  
+COPY entrypoint.sh /entrypoint
+RUN mkdir /root/airflow && mkdir /airflow
+COPY airflow.cfg /root/airflow
+RUN chmod +x /entrypoint/entrypoint.sh 
 
 WORKDIR /airflow
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,56 +1,114 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+echo "Validating required params..."
+
+if [ -z $USE_CONSUL ]; then
+  echo "APP_ENV is missing"
+  exit 1
+fi
+
+if ($USE_CONSUL); then 
+    if [ -z $APP_ENV ]; then
+    echo "APP_ENV is missing"
+    exit 1
+    fi
+
+    if [ -z $ROOT_APP_FOLDER ]; then
+    echo "ROOT_APP_FOLDER is missing"
+    exit 1
+    fi
+
+    if [ -z $CONSUL_HTTP_ADDR ]; then
+    echo "CONSUL_HTTP_ADDR is missing"
+    exit 2
+    fi
+
+    if [ -z $CONSUL_HTTP_TOKEN ]; then
+    echo "CONSUL_HTTP_TOKEN is missing"
+    exit 3
+    fi
+fi
+
 export AIRFLOW_HOME="/airflow"
+export AIRFLOW_CONFIG=/root/$AIRFLOW_HOME/airflow.cfg
 export DAG_FOLDER="dags"
-export VARIABLE_FILE="variables.json"
 
-airflow db init
+function readAndImportVar {
+    filevar=$1
+    fileContent=$(sed -e "s/{DAG}/$filevar/g" /entrypoint/variables.tmpl) ; echo $fileContent > /entrypoint/$filevar.tmpl
+    fileContent=$(sed -e "s/{ROOT_APP_FOLDER}/$ROOT_APP_FOLDER/g" /entrypoint/$filevar.tmpl) ; echo $fileContent > /entrypoint/$filevar.tmpl
+    fileContent=$(sed -e "s/{APP_ENV}/$APP_ENV/g" /entrypoint/$filevar.tmpl) ; echo $fileContent > /entrypoint/$filevar.tmpl
 
-echo "******** before Import variables $file"
-
-echo "******** before Import variables $VARIABLE_FILE"
-
-if [ ! -z $VARIABLE_FILE ] ; then
-    echo ">>>>>>>> VAraible llena" 
-    if [ -f "$AIRFLOW_HOME/variables/$VARIABLE_FILE" ] ; then
-        echo "******** Import variables $VARIABLE_FILE"
-        airflow variables import $AIRFLOW_HOME/variables/$VARIABLE_FILE
-    fi;
-#else
-#   echo "ERROR: A variable file was defined an it doesn't exist"
-#    exit 1
-fi
-
- echo "******** After Import variables $file"
-
-a=0;
-for file in $(ls $AIRFLOW_HOME/$DAG_FOLDER/*.py); do 
-    echo "******** Execute lint on $file"
-    flake8  --ignore E501 $file
-    echo "******** End of lint on $file"
-
-    echo "******** Execute python on $file"
-    python $file ; 
-    if [[ $? == 1 ]] ; then
-        a=1;
+    /entrypoint/consul-template -template="/entrypoint/$filevar.tmpl:/entrypoint/${filevar}_variable.json" -config /entrypoint/config.hcl -once -log-level info
+    if ([ -f "/entrypoint/${filevar}_variable.json" ] && [[ ! -z $(grep '[^[:space:]]' "/entrypoint/${filevar}_variable.json") ]]); then
+        airflow variables import /entrypoint/${filevar}_variable.json
     fi
-    echo "******** End python on $file"
+}
+
+
+airflow db reset --y 
+airflow users create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
+
+if ($USE_CONSUL); then
+    for filevar in $(ls "$AIRFLOW_HOME/dags/"); do
+        echo "******** Import variables $filevar"
+        readAndImportVar "$filevar"
+    done; 
+    echo "******** Import variables common"
+    readAndImportVar "common"
+fi
     
-    echo "******** Execute  black on $file"
-    pytest $file --black -v  -W ignore::DeprecationWarning
-    echo "******** End of lint on $file"
-done
 
-if [ -z "$TESTS_FOLDER" ] ; then
-    echo "******** Execute test on all dags in folder"
-    pytest $AIRFLOW_HOME/$DAG_FOLDER/$TESTS_FOLDER/*.py -v -W ignore::DeprecationWarning
-    if [[ $? == 1 ]] ; then
-        a=1;
-    fi
+airflow scheduler -n=1
+airflow dags list-import-errors
 
-    if [[ $a == 1 ]] ; then
-        echo "There are tests that failed"
-    fi
-fi
+# echo "******** before Import variables $file"
+
+# echo "******** before Import variables $VARIABLE_FILE"
+
+# if [ ! -z $VARIABLE_FILE ] ; then
+#     echo ">>>>>>>> Varaible llena" 
+#     if [ "find . -name variables.json" ] ; then
+#         echo "******** Import variables $VARIABLE_FILE"
+#         airflow variables import $AIRFLOW_HOME/variables/$VARIABLE_FILE
+#     fi;
+# #else
+# #   echo "ERROR: A variable file was defined an it doesn't exist"
+# #    exit 1
+# fi
+
+#  echo "******** After Import variables $file"
+
+# a=0;
+# for file in $(ls $AIRFLOW_HOME/$DAG_FOLDER/*.py); do 
+#     echo "******** Execute lint on $file"
+#     flake8  --ignore E501 $file
+#     echo "******** End of lint on $file"
+
+#     echo "******** Execute python on $file"
+#     python $file ; 
+#     if [[ $? == 1 ]] ; then
+#         a=1;
+#     fi
+#     echo "******** End python on $file"
+    
+#     echo "******** Execute  black on $file"
+#     pytest $file --black -v  -W ignore::DeprecationWarning
+#     echo "******** End of lint on $file"
+# done
+
+# if [ -z "$TESTS_FOLDER" ] ; then
+#     echo "******** Execute test on all dags in folder"
+#     pytest $AIRFLOW_HOME/$DAG_FOLDER/$TESTS_FOLDER/*.py -v -W ignore::DeprecationWarning
+#     if [[ $? == 1 ]] ; then
+#         a=1;
+#     fi
+
+#     if [[ $a == 1 ]] ; then
+#         echo "There are tests that failed"
+#     fi
+# fi
 
 exit $a
 
